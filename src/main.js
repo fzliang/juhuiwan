c5feb0616f160a5128778d59dc94a1944c989acf
fzliang <cnfzliang@163.com>
2017-09-25 23:05:01 +0800

    修改axios 5000ms

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import '../static/reset.css'
 
 Vue.prototype.$http = axios.create({
   baseURL: 'http://47.95.10.5:8888',
-  timeOut: 1000,
+  timeOut: 5000,
   paramsSerializer: function(params) {
     return Qs.stringify(params, {arrayFormat: 'brackets'})
   }
